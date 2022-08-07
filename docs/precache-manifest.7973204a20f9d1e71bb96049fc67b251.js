fe26fc1a030f6d8e9283ec6d7869f7be63a2fe34
vik <superapollo56@protonmail.com>
2022-08-07 02:29:53 -0300

    Added docs

---
+++ docs/precache-manifest.7973204a20f9d1e71bb96049fc67b251.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7289930185381a382a8c",
+    "url": "css/app.5e422ddc.css"
+  },
+  {
+    "revision": "6cf071566d73aa60edfe8a323164736d",
+    "url": "fonts/mary.6cf07156.ttf"
+  },
+  {
+    "revision": "13df5165014ab59dbe57e40f3551b23f",
+    "url": "img/brand-css3.13df5165.svg"
+  },
+  {
+    "revision": "d392354aba0659a03b0fa8a8f8abf2a8",
+    "url": "img/home.d392354a.svg"
+  },
+  {
+    "revision": "ff4f0b5fd55ee2a59de25cb4ce5d514f",
+    "url": "img/keyboard.ff4f0b5f.svg"
+  },
+  {
+    "revision": "de84712530ff6380bad10183b50abccc",
+    "url": "img/logo.de847125.svg"
+  },
+  {
+    "revision": "1cd3b5102cda3bab85e06538ab83d37f",
+    "url": "img/palette.1cd3b510.svg"
+  },
+  {
+    "revision": "f393a6813c6ad085c94a872fb06d178e",
+    "url": "img/play-card.f393a681.svg"
+  },
+  {
+    "revision": "2f72163fdc7cee0c8a71b54c856212a7",
+    "url": "img/tech-box.2f72163f.svg"
+  },
+  {
+    "revision": "7a46022d1e4f5cb2c80cce34ec464a90",
+    "url": "index.html"
+  },
+  {
+    "revision": "7289930185381a382a8c",
+    "url": "js/app.4b48b5e0.js"
+  },
+  {
+    "revision": "337e974b1da648f63fb8",
+    "url": "js/chunk-vendors.2126ba50.js"
+  },
+  {
+    "revision": "7a8bf79ecfc457b6122de5f42c01f1f3",
+    "url": "manifest.json"
+  }
+]);
